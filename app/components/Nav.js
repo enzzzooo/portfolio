19aca8902317812dd24579042dd1a5b2072aa2d8
enzzzooo <ed123658@gmail.com>
2023-10-16 10:12:45 -0500

    más

---
+++ app/components/Nav.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import s from "./nav.module.css";
+
+export default function Header() {
+  return (
+    <header className={s.header}>
+      <nav className={s.nav}>
+        <Link className={s.logo} href="/">
+          My Website
+        </Link>
+        <ul className={s.menu}>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
