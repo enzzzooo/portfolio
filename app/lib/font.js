f3f4649dfa49142bae76809913689a1c8b86d59b
enzzzooo <ed123658@gmail.com>
2023-10-16 21:46:59 -0500

    ferda

--- app/lib/font.js
+++ app/lib/font.js
@@ -1,3 +1,48 @@
 import { Inter } from "next/font/google";
-
+import localFont from "next/font/local";
 export const inter = Inter({ subsets: ["latin"] });
+export const gt_america = localFont({
+  src: [
+    {
+      path: "./GT-America-Mono-Bold-Trial.woff2",
+      weight: "800",
+      style: "normal",
+    },
+
+    {
+      path: "./GT-America-Mono-Black-Trial.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./GT-America-Expanded-Bold-Trial.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "./GT-America-Expanded-Black-Trial.woff2",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./GT-America-Mono-Bold-Italic-Trial.woff2",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "./GT-America-Mono-Black-Italic-Trial.woff2",
+      weight: "900",
+      style: "italic",
+    },
+    {
+      path: "./GT-America-Expanded-Bold-Italic-Trial.woff2",
+      weight: "800",
+      style: "italic",
+    },
+    {
+      path: "./GT-America-Expanded-Black-Italic-Trial.woff2",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+});
--- app/page.js
+++ app/page.js
@@ -1,5 +1,19 @@
 import s from "./page.module.css";
+import { gt_america } from "./lib/font";
+import Image from "next/image";
 export default function Home() {
   // home is descriptive to programmer; can also be page or another word
-  return <main></main>;
+  return (
+    <main>
+      <section className={s.section1}>
+        <div className={`${gt_america.className} ${s.textWrapper}`}>
+          <h1 className={s.h1Title}>The Website</h1>
+          <h2 className={s.h2Title}>My website is grand</h2>
+        </div>
+        <div className={s.imageWrapper}>
+          <Image src={}></Image>
+        </div>
+      </section>
+    </main>
+  );
 }
