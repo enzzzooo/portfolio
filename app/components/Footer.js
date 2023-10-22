56f2945f528d29cff8e3fa50b8efb410ebdcc624
enzzzooo <ed123658@gmail.com>
2023-10-22 10:50:28 -0500

    Working, just have to put stars at same level

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,18 +3,21 @@ import Link from "next/link";
 import Star from "./Star";
 export default function Footer() {
   return (
-    <div className={s.wrapper}>
-      <h1 className={s.main}>Main</h1>
-      <h2>Rarw</h2>
-      <ul>
-        <li>
-          <Link href="../portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link href="../about">About</Link>
-        </li>
-        <li>Copyright @ 2023</li>
-      </ul>
-    </div>
+    <>
+      <Star />
+      <div className={s.wrapper}>
+        <h1 className={s.main}>Main</h1>
+        <h2>Rarw</h2>
+        <ul>
+          <li>
+            <Link href="../portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link href="../about">About</Link>
+          </li>
+          <li>Copyright @ 2023</li>
+        </ul>
+      </div>
+    </>
   );
 }
