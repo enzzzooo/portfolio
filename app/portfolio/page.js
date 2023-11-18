bf6a8acb53e3cc4da4f2cde62e1fba61a76097dc
Enzo Dorfman <46726911+enzzzooo@users.noreply.github.com>
2023-11-18 04:14:55 +0000

    rename

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,5 +1,4 @@
 import s from "../portfolio/page.module.css";
-import Star from "../components/Star";
 export default function page() {
   return <section className={`top ${s.t}`}>Hi how's it going</section>;
 }
